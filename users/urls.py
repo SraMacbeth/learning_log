3e2c450d9ebcf6e0605e92c96ff7efe01b8b61c0
Emilia <emiliapol81@gmail.com>
2024-07-19 22:13:15 -0300

    Modificando urls.py en users

--- users/urls.py
+++ users/urls.py
@@ -8,11 +8,11 @@ app_name = 'users'
 
 urlpatterns = [
     # Login page
-    path('login/$', LoginView.as_view(template_name ='users/login.html'),name='login'), 
+    path('login/', LoginView.as_view(template_name ='users/login.html'),name='login'), 
 
     # Logout page
-    path('logout/$', views.logout_view, name='logout'), 
+    path('logout/', views.logout_view, name='logout'), 
 
     # Registration page
-    path('register/$', views.register, name='register'), 
+    path('register/', views.register, name='register'), 
 ]   
